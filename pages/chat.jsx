f96241261d1d93eef409765465ed845062d19ad5
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-03-18 18:34:27 -0400

    Trick fix for chat scroll issues

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -1,8 +1,13 @@
 import ChatPage from "@pages/MessagesFeature/ChatPage";
 import withAuth from "src/helpers/withAuth";
+import EmptyLayout from "src/layouts/EmptyLayout";
 
 function Chat() {
   return <ChatPage />;
 }
 
-export default withAuth(Chat);
+const WithAuthChat = withAuth(Chat);
+
+// WithAuthChat.layout = EmptyLayout;
+
+export default WithAuthChat;
