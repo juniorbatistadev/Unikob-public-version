9ea2cc070a05311249fed2c6450b622c1dabb7ee
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-03-21 01:09:21 -0400

    description

--- src/features/SchoolFeature/CreateSchoolPage/index.jsx
+++ src/features/SchoolFeature/CreateSchoolPage/index.jsx
@@ -4,6 +4,7 @@ import GoBackButton from "@components/common/GoBackButton";
 import Title from "@components/common/Title";
 import CreateSchoolForm from "./CreateSchoolForm";
 import styles from "./index.module.css";
+import Text from "@components/common/Text";
 
 export default function CreateSchoolPage() {
   return (
@@ -11,6 +12,11 @@ export default function CreateSchoolPage() {
       <FlexRow>
         <GoBackButton /> <Title text="Agregar Escuela" margin="20px 5px" />
       </FlexRow>
+      <Text
+        margin={10}
+        text="ATENCION: no podras modificar los datos luego que esten guardados. Debes asegurate que esten correctos."
+      />
+
       <CreateSchoolForm />
     </FlexColumn>
   );
