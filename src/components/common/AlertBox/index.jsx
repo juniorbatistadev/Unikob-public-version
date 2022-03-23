327bf31627993862117f325b3a70cf60bbbb8606
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-03-23 00:25:02 -0400

    bugs fixes 2

--- src/components/common/AlertBox/index.jsx
+++ src/components/common/AlertBox/index.jsx
@@ -1,12 +1,7 @@
 import styles from "./index.module.css";
 
 function AlertBox({ type, text }) {
-  return (
-    <div className={styles[type]}>
-      <i class="fa fa-warning"></i>
-      {text}
-    </div>
-  );
+  return <div className={styles[type]}>{text}</div>;
 }
 
 export default AlertBox;
