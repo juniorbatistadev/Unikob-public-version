847a71fa01b125896a4618ba5fab3e9a196d995f
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-08-02 18:35:19 -0400

    search sheme

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,39 @@
+import Head from "next/head";
 import HomePage from "src/features/HomePage";
 import EmptyLayout from "src/layouts/EmptyLayout";
+import { SEARCH_PATH } from "src/paths";
 
 function Home() {
-  return <HomePage />;
+  function addSearchJsonLd() {
+    return {
+      __html: ` {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "url": "${process.env.NEXT_PUBLIC_APP_SITE_URL}",
+        "potentialAction": {
+          "@type": "SearchAction",
+          "target": {
+            "@type": "EntryPoint",
+            "urlTemplate": "${process.env.NEXT_PUBLIC_APP_SITE_URL}${SEARCH_PATH}?s={search_term_string}"
+          },
+          "query-input": "required name=search_term_string"
+        }
+      }
+  `,
+    };
+  }
+  return (
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addSearchJsonLd()}
+          key="search-jsonld"
+        />
+      </Head>
+      <HomePage />
+    </>
+  );
 }
 
 Home.layout = EmptyLayout;
