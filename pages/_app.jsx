a99b5cc2f77aa3592c4cefdac43221729be4e649
Junior Batista <31187922+juniorbatistaRD@users.noreply.github.com>
2022-08-04 18:56:31 -0400

    google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import AuthContextProvider from "src/contexts/AuthContext";
 import MainLayout from "src/layouts/MainLayout";
 import Head from "next/head";
 import { initFirebase } from "../initFirebase";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
   //checking if env is browser
@@ -27,6 +28,21 @@ function App({ Component, pageProps }) {
       <Head>
         <title>Unikob</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </Script>
       </Head>
       <Layout>
         <Component {...pageProps} />
