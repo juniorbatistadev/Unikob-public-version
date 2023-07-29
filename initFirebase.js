6a1ea301d3c64aec20b39cc3898c3d2e9b64d2b6
Junior <juniorbatistadev@gmail.com>
2023-07-29 15:25:17 -0400

    fixed creds

--- initFirebase.js
+++ initFirebase.js
@@ -3,13 +3,13 @@ import { getMessaging, getToken } from "firebase/messaging";
 
 export const initFirebase = () => {
   const firebaseConfig = {
-    apiKey: "AIzaSyDI0Jb4q-cepyjf3aAR3VRb1xDD1ycHn8k",
-    authDomain: "unikob-43fd8.firebaseapp.com",
-    projectId: "unikob-43fd8",
-    storageBucket: "unikob-43fd8.appspot.com",
-    messagingSenderId: "468451085235",
-    appId: "1:468451085235:web:11e886d80056dd7e1519c6",
-    measurementId: "G-XG05TBZ8PL",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   };
 
   // Initialize Firebase
